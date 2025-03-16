d64ab3e70a66903a5d6bdb30c80f30a81078927a
Оглашевич Саша <sashaoglashevichi@gmail.com>
2025-03-17 01:39:50 +0200

    Добавьте файлы проекта.

---
+++ Lab5Excel/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Lab5Excel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            writetoexcel = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            richTextBox1 = new RichTextBox();
+            txtExcelSum = new TextBox();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // writetoexcel
+            // 
+            writetoexcel.Location = new Point(19, 38);
+            writetoexcel.Name = "writetoexcel";
+            writetoexcel.Size = new Size(217, 41);
+            writetoexcel.TabIndex = 0;
+            writetoexcel.Text = "1. Write to excel";
+            writetoexcel.UseVisualStyleBackColor = true;
+            writetoexcel.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(325, 38);
+            button2.Name = "button2";
+            button2.Size = new Size(217, 41);
+            button2.TabIndex = 1;
+            button2.Text = "2. ReadFromExcel";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(19, 163);
+            button3.Name = "button3";
+            button3.Size = new Size(203, 41);
+            button3.TabIndex = 2;
+            button3.Text = "1. Text to grid";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(325, 163);
+            button4.Name = "button4";
+            button4.Size = new Size(203, 41);
+            button4.TabIndex = 3;
+            button4.Text = "2. Grid to excel";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(593, 163);
+            button5.Name = "button5";
+            button5.Size = new Size(203, 41);
+            button5.TabIndex = 4;
+            button5.Text = "3. Excel to grid";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(593, 38);
+            button6.Name = "button6";
+            button6.Size = new Size(203, 41);
+            button6.TabIndex = 5;
+            button6.Text = "4. Grid To Text File";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(593, 97);
+            button7.Name = "button7";
+            button7.Size = new Size(203, 41);
+            button7.TabIndex = 6;
+            button7.Text = "5. WrlnCellsExcel";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(122, 107);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(428, 107);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(38, 110);
+            label1.Name = "label1";
+            label1.Size = new Size(48, 15);
+            label1.TabIndex = 9;
+            label1.Text = "txtWrite";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(339, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 15);
+            label2.TabIndex = 10;
+            label2.Text = "txtRead";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(19, 244);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(331, 433);
+            dataGridView1.TabIndex = 11;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(458, 236);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(325, 441);
+            dataGridView2.TabIndex = 12;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(882, 38);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(506, 588);
+            richTextBox1.TabIndex = 13;
+            richTextBox1.Text = "";
+            // 
+            // txtExcelSum
+            // 
+            txtExcelSum.Location = new Point(1000, 654);
+            txtExcelSum.Name = "txtExcelSum";
+            txtExcelSum.Size = new Size(100, 23);
+            txtExcelSum.TabIndex = 14;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(882, 657);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 15);
+            label3.TabIndex = 15;
+            label3.Text = "txtExcelSum";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1429, 793);
+            Controls.Add(label3);
+            Controls.Add(txtExcelSum);
+            Controls.Add(richTextBox1);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(writetoexcel);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button writetoexcel;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private RichTextBox richTextBox1;
+        private TextBox txtExcelSum;
+        private Label label3;
+    }
+}
---
+++ Lab5Excel/Form1.cs
@@ -0,0 +1,266 @@
+﻿using OfficeOpenXml;
+
+namespace Lab5Excel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\Computer\Desktop\proba.xlsx";
+
+            FileInfo file = new FileInfo(filePath);
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+
+            using (ExcelPackage excelPackage = new ExcelPackage(file))
+            {
+                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.FirstOrDefault();
+
+                if (worksheet == null)
+                {
+                    worksheet = excelPackage.Workbook.Worksheets.Add("Sheet1");
+                }
+
+                worksheet.Cells[1, 1].Value = textBox1.Text;  // Scrie valoarea în celula A1
+                excelPackage.Save();
+            }
+            richTextBox1.AppendText("Date scrise în Excel cu succes!\n");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\Computer\Desktop\proba.xlsx";
+            FileInfo file = new FileInfo(filePath);
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // Setare licență EPPlus
+
+            if (!file.Exists)
+            {
+                MessageBox.Show("Fișierul Excel nu există!");
+                return;
+            }
+
+            using (ExcelPackage excelPackage = new ExcelPackage(file))
+            {
+                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.FirstOrDefault();
+
+                if (worksheet == null)
+                {
+                    MessageBox.Show("Nu există foi de calcul în fișierul Excel!");
+                    return;
+                }
+
+                object readValue = worksheet.Cells[1, 1].Value;  // Citește valoarea din celula A1
+                textBox2.Text = readValue != null ? readValue.ToString() : "Celula este goală";
+
+                richTextBox1.AppendText("Date citite din Excel cu succes!\n");
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\Computer\Desktop\data.txt";
+
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("Fișierul .txt nu există!");
+                return;
+            }
+
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            dataGridView1.Columns.Add("ProdID", "ProdID");
+            dataGridView1.Columns.Add("Date1", "Date1");
+            dataGridView1.Columns.Add("QuantS", "QuantS");
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                string[] values = line.Split(',');
+
+                if (values.Length == 3) // Asigură că sunt 3 coloane
+                {
+                    dataGridView1.Rows.Add(values[0], values[1], values[2]);
+                }
+            }
+
+            richTextBox1.AppendText("DataGridView a fost completat cu datele din txt file\n");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\Computer\Desktop\proba.xlsx";
+            FileInfo file = new FileInfo(filePath);
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // Setare licență EPPlus
+
+            using (ExcelPackage excelPackage = new ExcelPackage(file))
+            {
+                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.FirstOrDefault();
+
+                if (worksheet == null)
+                {
+                    worksheet = excelPackage.Workbook.Worksheets.Add("Sheet1");
+                }
+
+                // Scriere antete în Excel
+                for (int col = 0; col < dataGridView1.Columns.Count; col++)
+                {
+                    worksheet.Cells[1, col + 1].Value = dataGridView1.Columns[col].HeaderText;
+                }
+
+                // Scriere date în Excel
+                for (int row = 0; row < dataGridView1.Rows.Count; row++)
+                {
+                    for (int col = 0; col < dataGridView1.Columns.Count; col++)
+                    {
+                        object value = dataGridView1.Rows[row].Cells[col].Value;
+                        worksheet.Cells[row + 2, col + 1].Value = value != null ? value.ToString() : "";
+                    }
+                }
+
+                excelPackage.Save();
+            }
+
+            richTextBox1.AppendText("Datele din DataGridView au fost salvate în Excel!\n");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\Computer\Desktop\test.txt";
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                // Scrie anteturile coloanelor
+                for (int col = 0; col < dataGridView2.Columns.Count; col++)
+                {
+                    writer.Write(dataGridView2.Columns[col].HeaderText);
+                    if (col < dataGridView2.Columns.Count - 1)
+                        writer.Write(","); // Separator CSV
+                }
+                writer.WriteLine();
+
+                // Scrie datele rând cu rând
+                foreach (DataGridViewRow row in dataGridView2.Rows)
+                {
+                    if (!row.IsNewRow) // Ignoră ultimul rând gol
+                    {
+                        for (int col = 0; col < dataGridView2.Columns.Count; col++)
+                        {
+                            writer.Write(row.Cells[col].Value?.ToString() ?? "");
+                            if (col < dataGridView2.Columns.Count - 1)
+                                writer.Write(",");
+                        }
+                        writer.WriteLine();
+                    }
+                }
+            }
+
+            richTextBox1.AppendText($"Datele din DataGridView au fost salvate în fișierul TXT! {filePath} \n");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\Computer\Desktop\proba.xlsx";
+
+            FileInfo file = new FileInfo(filePath);
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            if (!file.Exists)
+            {
+                MessageBox.Show("Fișierul Excel nu există!");
+                return;
+            }
+
+            using (ExcelPackage excelPackage = new ExcelPackage(file))
+            {
+                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.FirstOrDefault();
+
+                if (worksheet == null)
+                {
+                    MessageBox.Show("Nu există foi de calcul în fișierul Excel!");
+                    return;
+                }
+
+                dataGridView2.Rows.Clear();
+                dataGridView2.Columns.Clear();
+
+                int rowCount = worksheet.Dimension.Rows;
+                int colCount = worksheet.Dimension.Columns;
+
+                // Adaugă coloanele pe baza antetelor din Excel
+                for (int col = 1; col <= colCount; col++)
+                {
+                    string header = worksheet.Cells[1, col].Value?.ToString() ?? $"Col{col}";
+                    dataGridView2.Columns.Add($"Col{col}", header);
+                }
+
+                // Adaugă rândurile cu date
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    object[] rowData = new object[colCount];
+                    for (int col = 1; col <= colCount; col++)
+                    {
+                        rowData[col - 1] = worksheet.Cells[row, col].Value?.ToString() ?? "";
+                    }
+                    dataGridView2.Rows.Add(rowData);
+                }
+            }
+
+            richTextBox1.AppendText("Datele din Excel au fost încărcate în DataGridView!\n");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\Computer\Desktop\proba.xlsx";
+            FileInfo file = new FileInfo(filePath);
+            Random rnd = new Random();
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (ExcelPackage excelPackage = new ExcelPackage(file))
+            {
+                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.FirstOrDefault();
+
+                if (worksheet == null)
+                {
+                    worksheet = excelPackage.Workbook.Worksheets.Add("Sheet1");
+                }
+
+                // scriem valori random in coloana J (coloana 10) de la J1 la J12
+                for (int row = 1; row <= 12; row++)
+                {
+                    worksheet.Cells[row, 10].Value = rnd.Next(10, 100);
+                }
+
+                // scriem formula de suma in celula data
+                worksheet.Cells[1, 12].Formula = "SUM(J1:J12)";
+
+                excelPackage.Save();
+
+                worksheet.Calculate(); // calculam formula in excel
+                object sumValue = worksheet.Cells[1, 12].Value;
+
+                txtExcelSum.Text = sumValue != null ? sumValue.ToString() : "0";
+            }
+
+            richTextBox1.AppendText("Valori random înscrise în Excel și suma calculată! \n");
+        }
+    }
+}
